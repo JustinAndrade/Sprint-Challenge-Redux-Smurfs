376d292e5635a13876dfebfebcbc9351dbbd8b30
JustinAndrade <j.andrade.john@gmail.com>
2019-04-20 13:48:22 -0400

    smurf list displayed

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import './App.css';
 
 import { getSmurfs } from '../actions';
+import SmurfList from './SmurfList';
 
 /*
  to wire this component up you're going to need a few things.
@@ -20,9 +21,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.smurfs.map((smurf, index) => 
+          <SmurfList smurf={smurf} key={index} />
+          )}
       </div>
     );
   }
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SmurfList = props => {
+    {console.log(props)}
+    return (
+        <div className='smurf-list'>
+            <h2>{props.smurf.name}</h2>
+            <p>{props.smurf.age}</p>
+            <p>{props.smurf.height}</p>
+        </div>
+    )
+}
+
+export default SmurfList;
